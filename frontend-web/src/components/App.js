36ea06cc936178695850cfc9b2c03e8a25ebec3c
Augusto Fabian Rodrigues <augustofabianrodrigues@gmail.com>
2021-01-20 20:25:17 -0300

    feat: Init frontend-web

---
+++ frontend-web/src/components/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <div className="h-screen-nav-fix w-screen font-noto-sans overflow-hidden bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-gray-50"></div>
+  );
+}
+
+export default App;
